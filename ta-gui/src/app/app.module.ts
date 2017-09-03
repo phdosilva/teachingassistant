dc03c7bb06ff9d2ad5eb24dab26916ea6566608f
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:03:47 -0300

    elementos do formulario para cadastro de aluno

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
