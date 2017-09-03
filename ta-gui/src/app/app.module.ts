1a31b1af4122fe4b75abd06dbfaf1d0838fbad58
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:13:56 -0300

    botao e funcionalidade do formulario para cadastro de alunos

--- ta-gui/src/app/app.module.ts
+++ ta-gui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
